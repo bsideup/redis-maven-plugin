fd3aa666dd53fdfbe20e9326d70f41c1b581c342
Denis N. Antonioli <denisa@sunrunhome.com>
2014-03-20 16:48:06 -0700

    Provide a parameter "redis.server.skip", default to false, similar to what other similar plugins (cargo, jetty, derby, ...) provide

--- src/main/java/ru/trylogic/maven/plugins/redis/ShutdownRedisMojo.java
+++ src/main/java/ru/trylogic/maven/plugins/redis/ShutdownRedisMojo.java
@@ -7,16 +7,24 @@ import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
 
 @Mojo( name = "shutdown", defaultPhase = LifecyclePhase.NONE )
 public class ShutdownRedisMojo  extends AbstractMojo {
-    
+
+    @Parameter(property = "redis.server.skip", defaultValue = "false")
+    public boolean skip;
+
     @Override
     @SuppressWarnings("unchecked")
     public void execute() throws MojoExecutionException, MojoFailureException {
+        if (skip) {
+            getLog().info("Redis server had been skipped...");
+            return;
+        }
 
         DefaultEventExecutorGroup redisGroup = (DefaultEventExecutorGroup) getPluginContext().get(RunRedisMojo.REDIS_GROUP_CONTEXT_PROPERTY_NAME);
-        
+
         if(redisGroup == null) {
             throw new MojoExecutionException("Redis server is not running");
         }
